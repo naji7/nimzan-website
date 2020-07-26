32997fb41817ee2fead71f91d75d780b3a8527de
naji7 <naji@Mohameds-MacBook.local>
2020-07-26 12:43:08 +0530

    load

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component, useRef, useEffect, useState } from "react";
+import React, { Component, useRef, useEffect, lazy, Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Layout from "./App/skeliton/layout";
+// import Layout from "./App/skeliton/layout";
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,26 +10,37 @@ import {
 } from "react-router-dom";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import Load from "./load";
+
+const Layout = lazy(() => import("./App/skeliton/layout"));
 
 const App = () => {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
+
   return (
-    <div>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"
-      ></meta>
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Layout />
-          </Route>
-        </Switch>
-      </Router>
-      ;
-    </div>
+    <Suspense
+      fallback={
+        <div>
+          <Load />
+        </div>
+      }
+    >
+      <div>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"
+        ></meta>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Layout />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Suspense>
   );
 };
 
---
+++ src/load.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./load.scss";
+
+export default class Load extends Component {
+  render() {
+    return (
+      <div className="load">
+        <svg>
+          <rect></rect>
+        </svg>
+      </div>
+    );
+  }
+}
